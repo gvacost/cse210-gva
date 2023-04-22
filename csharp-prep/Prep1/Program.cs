5bfa029887bf767f29643bb1e11ee451b8ef0146
gvacost <gonza.96.acosta@gmail.com>
2023-04-22 07:51:11 -0300

    Assignment 1-1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep1 World!");
+//     }
+// }
+
+Console.Write("What is your first name? ");
+string first_n = Console.ReadLine();
+
+Console.Write("What is your last name? ");
+string last_n = Console.ReadLine();
+
+Console.WriteLine($"Your name is {last_n}, {first_n} {last_n}.");
