b2504687dae806be344864d2ccf70b70d92717de
gvacost <gonza.96.acosta@gmail.com>
2023-04-24 20:56:47 -0300

    first core requirement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage");
+        string grade = Console.ReadLine();
+        float grade_p =float.Parse(grade);
+        if (grade_p>=90)
+        {
+            Console.WriteLine("Then your grade is A");    
+        }
+        else if (grade_p>=80 )
+        {
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if (grade_p>=70)
+        {
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if (grade_p>=60)
+        {
+            Console.WriteLine("Your letter grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F");
+        }
+
+         
     }
 }
