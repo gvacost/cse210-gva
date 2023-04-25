894e8d7a4c09157eda933a3c65bde113d9e362c3
gvacost <gonza.96.acosta@gmail.com>
2023-04-24 21:14:07 -0300

    second and third core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,31 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.Write("What is your grade percentage");
+        Console.Write("What is your grade percentage: ");
         string grade = Console.ReadLine();
         float grade_p =float.Parse(grade);
+        string l_grade = "";
         if (grade_p>=90)
         {
-            Console.WriteLine("Then your grade is A");    
+            l_grade ="A";
+            // Console.WriteLine("Your letter grade is A");    
         }
         else if (grade_p>=80 )
         {
-            Console.WriteLine("Your letter grade is B");
+            l_grade = "B";
+            // Console.WriteLine("Your letter grade is B");
         }
         else if (grade_p>=70)
         {
-            Console.WriteLine("Your letter grade is C");
+            l_grade = "C";
+            // Console.WriteLine("Your letter grade is C");
         }
         else if (grade_p>=60)
         {
-            Console.WriteLine("Your letter grade is D");
+            l_grade = "D";
+            // Console.WriteLine("Your letter grade is D");
         }
         else
         {
-            Console.WriteLine("Your letter grade is F");
+            l_grade = "F";
+            // Console.WriteLine("Your letter grade is F");
         }
 
-         
+        Console.WriteLine($"Your letter grade is {l_grade}");
+        
+        if (grade_p > 70)
+        {
+            Console.WriteLine("Congratulations! You´re amazing");
+        }
+        else
+        {
+            Console.WriteLine("Don´t worry the next time you will do much better. See you soon!");
+        }
     }
 }
