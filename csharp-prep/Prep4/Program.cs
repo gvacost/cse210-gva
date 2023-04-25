4359df42a17f6f11cd9f6f23421f98c72e101079
gvacost <gonza.96.acosta@gmail.com>
2023-04-24 22:34:27 -0300

    first core req

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Enter a number: ");
+        string num = Console.ReadLine();
+        float number = float.Parse(num);
+        List <float> numbers = new List<float>();
+        
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number: ");
+            num = Console.ReadLine();
+            number = float.Parse(num);
+        }
+        
+        Console.WriteLine($"The sum is {numbers.Sum()}");
+        // Console.WriteLine($"The average is {numbers.Average()}");
     }
 }
