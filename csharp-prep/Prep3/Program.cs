941d0c474f1e241e7b54cf2caa493984c679495c
gvacost <gonza.96.acosta@gmail.com>
2023-04-24 21:45:23 -0300

    first core req

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,27 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string number = Console.ReadLine();
+        int m_number = int.Parse(number);
+        Console.Write("What is your guess? ");
+        string g_number = Console.ReadLine();
+        int g_number_ = int.Parse(g_number);
+
+        if (g_number_ > m_number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (m_number> g_number_)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You got it!");
+        }
+
     }
 }
