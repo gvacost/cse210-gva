0d161ef742cf435080a4d8b49ff08f15d3cc1694
gvacost <gonza.96.acosta@gmail.com>
2023-04-25 19:23:25 -0300

    prep5 final requirement

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displaymessage();
+        string username = promptusername();
+        int usernumber = promptusernumber();
+        int squared =  square(usernumber);
+        displayresult(username, squared);
+    }
+    static void displaymessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string promptusername()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int promptusernumber()
+    {
+        Console.Write("What is your number? ");
+        string num = Console.ReadLine(); 
+        int number = int.Parse(num);
+
+        return number;
+    }
+
+    static int square (int number)
+    {
+        
+        int sq_number = number * number;
+
+        return sq_number;
+
+    }
+
+    static void displayresult(string name, int sq_number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sq_number}");
     }
 }
+
+// Finally I follow the sample solution because I could not find my mistake
