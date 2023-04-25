e416b071dfb2dd3ef6837a89887fb0c64d75c765
gvacost <gonza.96.acosta@gmail.com>
2023-04-24 21:55:48 -0300

    second core req

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -11,19 +11,27 @@ class Program
         Console.Write("What is your guess? ");
         string g_number = Console.ReadLine();
         int g_number_ = int.Parse(g_number);
-
-        if (g_number_ > m_number)
+        while (g_number_ != m_number)
         {
-            Console.WriteLine("Lower");
-        }
-        else if (m_number> g_number_)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
+            if (g_number_ > m_number)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                g_number = Console.ReadLine();
+                g_number_ = int.Parse(g_number);
+            }
+            else if (m_number> g_number_)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                g_number = Console.ReadLine();
+                g_number_ = int.Parse(g_number);
+            }
+            
+        } 
         {
             Console.WriteLine("You got it!");
-        }
+        }   
 
     }
 }
