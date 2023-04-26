9b482cb8403bf2b108502ae91164503dec733757
gvacost <gonza.96.acosta@gmail.com>
2023-04-26 19:10:58 -0300

    xz

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 0;
+        job1._endYear = 0;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 0;
+        job2._endYear = 0;
     }
 }
