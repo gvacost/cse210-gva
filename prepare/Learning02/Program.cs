f077ea34ea99d6e2b6bab5d81e0e861feff90b90
gvacost <gonza.96.acosta@gmail.com>
2023-04-26 20:00:05 -0300

    learning activity (late)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,13 +7,23 @@ class Program
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 0;
-        job1._endYear = 0;
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._company = "Apple";
-        job2._jobTitle = "Software Engineer";
-        job2._startYear = 0;
-        job2._endYear = 0;
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myresume = new Resume();
+        myresume._name ="Gonzalo Acosta";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
+
+
+
     }
 }
